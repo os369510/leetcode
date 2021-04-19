aaebed5404c816541f1ccefe2b782cd3542589bf
Jeremy Szu <jeremy.szu@canonical.com>
2021-04-20 00:21:06 +0800

    1512_Number_of_Good_Pairs

---
+++ 1512_Number_of_Good_Pairs/api.c
@@ -0,0 +1,10 @@
+int numIdenticalPairs(int* nums, int numsSize){
+    int count = 0, i = 0, j = 0;
+    for (i = 0; i < numsSize - 1; i++) {
+        for (j = i + 1; j < numsSize; j++) {
+            if (nums[i] == nums[j])
+                count++;
+        }
+    }
+    return count;
+}
