fa6f8b9d23ce45687ea0186828a69559fd63f711
Jeremy Szu <jeremy.szu@canonical.com>
2022-08-15 19:38:46 +0800

    153_Find_Minimum_in_Rotated_Sorted_Array

---
+++ 153_Find_Minimum_in_Rotated_Sorted_Array/api.c
@@ -0,0 +1,19 @@
+int findMin(int* nums, int numsSize){
+    int l = 0, r = numsSize - 1, m = 0;
+    while (l < r) {
+        m = (l + r) / 2;
+        if ((nums[l] <= nums[m]) && (nums[m] <= nums[r]))
+            break;
+        if (nums[l] > nums[m] && r != m) {
+            r = m;
+            continue;
+        }
+        if (nums[m] > nums[r] && l != m) {
+            l = m;
+            continue;
+        }
+        if (nums[l] > nums[r])
+            l = r;
+    }
+    return nums[l];
+}
